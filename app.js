b38d11d41c8f689e8eca2d5c19acc441d8148951
Aaira Khan <aairasalmankhan2008@gmail.com>
2024-12-03 14:28:58 +0500

    task done

---
+++ app.js
@@ -0,0 +1,22 @@
+var myName = "Aaira Khan";
+alert (myName);
+
+var message = "!!...Hello World...!!"
+alert (message);
+
+var abouta = "After HTML, CSS & bootstrap, I'm learning Javascript in my web-development course"
+alert (abouta)
+
+var aboutb = "My age is 16 and studying in XI. Along with being a student of biology, I'm also interested in Website Development skills!"
+alert (aboutb)
+
+var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert (pizza)
+
+var email = "My E-mail address is aairasalmankhan2008@gmail.com"
+alert (email)
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert (design)
+
+document.write ("Welcome To JS 2nd Assignment");
